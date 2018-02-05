8c81657202182e0adf777d6a254bdac97cb8b96b
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-05 11:50:56 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -36,3 +36,10 @@ end
 def sum_array(array)
   array.inject{|a,b| a+b}
 end
+
+def add_s(array)
+ array.each_with_index.collect {|element, index| 
+   if index != 1
+     element = element + "s"
+ }
+end
